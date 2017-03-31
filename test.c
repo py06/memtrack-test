e994c59de337c16ed898bc2f62bf6554b47df2de
Pierre-Yves Kerbrat <pkerbrat@free.fr>
2017-03-31 10:23:02 +0200

    test: add mmap test
    Signed-off-by: Pierre-Yves Kerbrat <pkerbrat@free.fr>

--- test.c
+++ test.c
@@ -2,8 +2,15 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <errno.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <math.h>
 
 void *alloc_ptr = NULL;
+void *mmap_ptr = NULL;
+char mmap_filename[256];
+int mmap_len = 0;
+int mmap_fd = 0;
 
 int menu()
 {
@@ -35,6 +42,42 @@ int test_free()
 	free(alloc_ptr);
 }
 
+int test_mmap()
+{
+	int addr = 0;
+	int pagesize = sysconf(_SC_PAGE_SIZE);
+
+	printf("filename =");
+	scanf("%s", &mmap_filename);
+	printf("len =");
+	scanf("%d", &mmap_len);
+
+
+	printf("pagesize=%d\n", pagesize);
+	printf("mmaplen=%d ceil=%d\n", mmap_len,
+	       (int)ceil((float)mmap_len / (float)pagesize) * pagesize);
+	mmap_len = (int)ceil((float)mmap_len / (float)pagesize) * pagesize;
+
+	mmap_fd = open(mmap_filename, O_RDONLY);
+	if (mmap_fd == -1) {
+		perror("open");
+		return -ENOENT;
+	}
+
+	mmap_ptr = mmap(NULL, mmap_len, PROT_READ, MAP_PRIVATE, mmap_fd, 0);
+	if (mmap_ptr == MAP_FAILED) {
+		perror("mmap");
+		return -ENOMEM;
+	}
+	return 0;
+}
+
+int test_munmap()
+{
+	munmap(mmap_ptr, mmap_len);
+	close(mmap_fd);
+}
+
 int main(int argc, char *argv[])
 {
 	int c;
@@ -47,6 +90,12 @@ int main(int argc, char *argv[])
 			break;
 		case 2:
 			test_free();
+			break;
+		case 3:
+			test_mmap();
+			break;
+		case 4:
+			test_munmap();
 			break;
 		default:
 			break;
